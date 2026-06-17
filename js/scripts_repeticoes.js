399fdfe1e950c2c7456f47fb3873b34a26d1348b
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-17 16:23:39 -0300

    alterada

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -81,3 +81,12 @@ presentes.forEach((elemento, i)=>{
     divForeach.innerHTML += `${i}{elemento} <br>`
 
 })
+
+const pessoas = [
+    {nome: 'Maria Flor', idade: 25, renda: 8500},
+    {nome: 'Joerdson Souza', idade: 75, renda: 5000},
+    {nome: 'Taoca', idade: 28, renda: 500},
+    {nome: 'Chicó', idade: 36, renda: 100},
+    {nome: 'João Grilo', idade: 32, renda: 80},
+    
+]
