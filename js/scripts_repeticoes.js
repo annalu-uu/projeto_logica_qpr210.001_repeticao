7091a96f48c1f61e9ae83237618fab94e9d086b2
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-17 14:49:40 -0300

    alterada

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -9,7 +9,7 @@ for (let i = 0; i < 10; i++) { // 'let' adicionado aqui
 divFor.innerHTML = resultadoFor; // Atualiza o DOM apenas uma vez
 
 
-// CONTADOR/ACUMULADOR
+//CONTADOR/ACUMULADOR
 const inputNum = document.querySelector('#num');
 const btnNum = document.querySelector('#btn-num');
 const divContAcum = document.querySelector('#div-cont-acum');
@@ -29,26 +29,28 @@ btnNum.addEventListener('click', (evt) => {
 });
 
 
-// CONTROLANDO FOR (Finalizado)
-const inputFrase = document.querySelector('#frase');
-const inputNumRepeticao = document.querySelector('#num-repeticao');
-const btnFrase = document.querySelector('#btn-frase');
-const divFrase = document.querySelector('#div-frase');
+//CONTROLANDO FOR (Finalizado)
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const btnFrase = document.querySelector('#btn-frase')
+const divFrase = document.querySelector('#div-frase')
 
-btnFrase.addEventListener('click', () => {
-    const frase = inputFrase.value;
-    const repeticoes = Number(inputNumRepeticao.value);
-    
-    let resultadoFrases = '';
+btnFrase.addEventListener('click', (evt) => {
+    let frase = inputFrase.value
+    let NumRepeti = Number(inputNumRepeticao.value)
 
-    // O For agora roda a quantidade de vezes que o usuário digitou no input!
-    for (let i = 0; i < repeticoes; i++) {
-        resultadoFrases += `${i + 1} - ${frase} <br>`;
-    }
+    for (let i = 0; i < numRepeti; i++) {
+       divFrase.innerHTML += `${i + 1} - ${frase} <br>`
 
-    divFrase.innerHTML = resultadoFrases;
-    
-    // Limpa os campos após o clique
-    inputFrase.value = '';
-    inputNumRepeticao.value = '';
-});
+    divFrase.innerHTML = resultadoFrases
+   }
+
+})
+
+//COLEÇÃO DE DADOS - ARRAY
+const presentes = ['Bicicleta', 'Celular', 'Camisa do Mengão' ]
+
+const divArray = document.querySelector('#div-array')
+
+//FOR IN
+
